69551c2ad03efd6ae3626c9ac202fd6b6df0bbe7
Paolo Bosetti <paolo.bosetti@unitn.it>
2015-02-27 18:19:38 +0100

    speed up of travis-ci build

--- run_test.rb
+++ run_test.rb
@@ -4,7 +4,7 @@ if __FILE__ == $PROGRAM_NAME
   require 'fileutils'
   FileUtils.mkdir_p 'tmp'
   unless File.exists?('tmp/mruby')
-    system 'git clone https://github.com/mruby/mruby.git tmp/mruby'
+    system 'git clone --depth 1 https://github.com/mruby/mruby.git tmp/mruby'
   end
   exit system(%Q[cd tmp/mruby; MRUBY_CONFIG=#{File.expand_path __FILE__} ./minirake #{ARGV.join(' ')}])
 end
@@ -12,5 +12,5 @@ end
 MRuby::Build.new do |conf|
   toolchain :clang
   conf.gembox 'default'
-  conf.gem :github => 'UniTN-mechatronics/mruby-ftp', :branch => 'master'
+  conf.gem File.dirname(__FILE__)
 end
